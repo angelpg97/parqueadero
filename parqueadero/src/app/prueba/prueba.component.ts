d0b9e0c2639fe52059343375a7d04b7f793a0cc7
angelpg97 <angelpg1997@outlook.com>
2025-03-26 14:59:51 -0500

    parqueadero
    arreglos a la interfaz

--- parqueadero/src/app/prueba/prueba.component.ts
+++ parqueadero/src/app/prueba/prueba.component.ts
@@ -6,11 +6,13 @@ import { RegistroComponent } from "../../public/registro/registro.component";
 import { EstacionamientoComponent } from "../../public/estacionamiento/estacionamiento.component";
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PagoComponent } from "../../public/pago/pago.component";
+import { InterfazeInicioComponent } from "../interfaze-inicio/interfaze-inicio.component";
 
 @Component({
   selector: 'app-prueba',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,NavegadorComponent, NavegadorVerticalComponent, IndexComponent, RegistroComponent, EstacionamientoComponent],
+  imports: [CommonModule, RouterOutlet, NavegadorComponent, NavegadorVerticalComponent, IndexComponent, RegistroComponent, EstacionamientoComponent, PagoComponent, InterfazeInicioComponent],
   templateUrl: './prueba.component.html',
   styleUrl: './prueba.component.scss'
 })
